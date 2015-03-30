41b565a67224b4e82ac22bcc3f753eab03ba2801
Bryan Baugher <bryan.baugher@cerner.com>
2015-03-29 19:18:16 -0500

    Updated supports in metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,7 @@ license          'All rights reserved'
 description      'Installs/Configures confluent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.2.0'
+
+%w{ ubuntu centos }.each do |os|
+  supports os
+end

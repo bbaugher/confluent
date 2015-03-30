bb29c06739b6ee707f5728773fcbf8c4b08fb9b8
Bryan Baugher <bryan.baugher@cerner.com>
2015-03-29 19:12:03 -0500

    Released 0.1.0 and bumped version to 0.2.0

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,4 @@ maintainer_email 'Bryan.Baugher@Cerner.com'
 license          'All rights reserved'
 description      'Installs/Configures confluent'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
-
-depends 'java'
+version          '0.2.0'
